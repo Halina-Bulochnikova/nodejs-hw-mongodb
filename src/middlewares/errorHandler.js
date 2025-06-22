115182e11baa1ade916f1d6432508e806ae91481
halina_bulochnikova <yatsynkagalina@gmail.com>
2025-06-22 20:47:50 +0200

    edited errors

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,9 +1,22 @@
 export const errorHandler = (err, req, res, next) => {
   const status = err.status || 500;
   
+  if (Array.isArray(err.errors)) {
+    const errors = err.errors.map(e => ({
+      field: e.path?.[0],
+      message: e.message
+    }));
+
+    return res.status(status).json({
+      status,
+      message: err.message,
+      errors
+    });
+  }
+
   res.status(status).json({
     status,
     message: 'Something went wrong',
-    data: err.message,
+    data: err.message
   });
 };
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,17 +4,39 @@ import Joi from 'joi';
 
 
 export const contactSchema = Joi.object({
-    name: Joi.string().min(3).max(20).required(),
-    phoneNumber: Joi.string().min(3).max(20).required(),
-    email: Joi.string().email().min(3).max(20).optional(),
-    contactType: Joi.string().valid("work", "home", "personal").required(),
-    isFavourite: Joi.boolean().optional(),
+    name: Joi.string().min(3).max(20).required()
+      .messages({
+      'string.base': '"name" must be a string',
+      'string.empty': '"name" is required',
+      'string.min': '"name" should have at least 3 characters',
+      'string.max': '"name" should have at most 20 characters',
+      'any.required': '"name" is required'
+    }),
+    phoneNumber: Joi.string().min(3).max(20).required()
+        .messages({
+        'string.empty': '"phoneNumber" is required',
+        'string.min': '"phoneNumber" should have at least 3 characters',
+        'string.max': '"phoneNumber" should have at most 20 characters',
+        'any.required': '"phoneNumber" is required'
+      }),
+    email: Joi.string().email().min(3).max(20).optional()
+        .messages({
+        'string.email': '"email" must be a valid email address',
+        'string.min': '"email" should have at least 3 characters',
+        'string.max': '"email" should have at most 20 characters',
+      }),
+    contactType: Joi.string().valid("work", "home", "personal").required()
+        .messages({
+        'any.only': '"contactType" must be one of [work, home, personal]',
+        'any.required': '"contactType" is required',
+      }),
+    isFavourite: Joi.boolean().optional()
+        .messages({
+        'boolean.base': '"isFavourite" must be true or false',
+      }),
 });
-export const updateContactSchema = Joi.object({
-    name: Joi.string().min(3).max(20),
-    phoneNumber: Joi.string().min(3).max(20),
-    email: Joi.string().email().min(3).max(20),
-    contactType: Joi.string().valid("work", "home", "personal"),
-    isFavourite: Joi.boolean(),
-  }).min(1);
+export const updateContactSchema = contactSchema.fork(
+    Object.keys(contactSchema.describe().keys),
+    (schema) => schema.optional())
+    .min(1);
   
