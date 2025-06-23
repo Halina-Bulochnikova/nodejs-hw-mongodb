2a919cfad0bd4974e0c33eb59fdf6696ac94f225
halina_bulochnikova <yatsynkagalina@gmail.com>
2025-06-23 23:51:00 +0200

    add 2 task

---
+++ src/db/models/user.js
@@ -0,0 +1,14 @@
+// src/db/models/user.js
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, unique: true, required: true, match: /\S+@\S+\.\S+/ },
+        password: { type: String, required: true }
+    },
+        {
+            timestamps: true,
+          },
+);
+export const UserCollection = model('user', userSchema);
