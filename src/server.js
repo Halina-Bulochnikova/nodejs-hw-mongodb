f8d1a94977599a5ae2b948f02d856426c6fe5ea5
halina_bulochnikova <yatsynkagalina@gmail.com>
2025-06-26 23:12:36 +0200

    add 4 task

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { contactSchema } from './validation/contacts.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -16,6 +17,7 @@ export const setupServer = () => {
 
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
   app.use(pino({ transport: { target: 'pino-pretty' } }));
   app.use('/contacts', router);
   app.use('/auth', authRouter);
@@ -40,6 +42,7 @@ export const setupServer = () => {
   app.use(notFoundHandler);
 
   app.use(errorHandler);
+  
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- src/services/auth.js
+++ src/services/auth.js
@@ -3,6 +3,9 @@
 import bcrypt from 'bcrypt';
 import { UserCollection } from '../db/models/user.js';
 import createHttpError from 'http-errors';
+import { SessionCollection } from '../db/models/session.js';
+import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/index.js';
+import { randomBytes } from 'crypto';
 
 export const registerUser = async (payload) => {
 
@@ -16,5 +19,28 @@ export const registerUser = async (payload) => {
   return await UserCollection.create({
     ...payload,
     password: encryptedPassword,
+  });
+};
+export const loginUser = async (payload) => {
+  const user = await UserCollection.findOne({ email: payload.email });
+  if (!user) {
+    throw createHttpError(401, 'User not found');
+  }
+  const isEqual = await bcrypt.compare(payload.password, user.password);
+
+  if (!isEqual) {
+    throw createHttpError(401, 'Unauthorized');
+  }
+  await SessionCollection.deleteMany({ userId: user._id });
+
+  const accessToken = randomBytes(15).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+
+  return await SessionCollection.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
   });
 };
