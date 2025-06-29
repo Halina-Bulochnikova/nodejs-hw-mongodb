4e6d476bc73b1146309c6c26184f508fe07b594c
halina_bulochnikova <yatsynkagalina@gmail.com>
2025-06-29 22:37:39 +0200

    add edit files

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -13,17 +13,16 @@ import { updateContactSchema, contactSchema } from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { authenticate } from '../middlewares/authenticate.js';
-import { checkRoles } from '../middlewares/checkRoles.js';
-import { ROLES } from '../constants/index.js';
+
 
 
 const router = Router();
 
 router.use(authenticate);
-router.get('/', checkRoles(ROLES.TEACHER), ctrlWrapper(getAllContactsController));
-router.get('/:contactId', checkRoles(ROLES.TEACHER, ROLES.PARENT), isValidId, ctrlWrapper(getContactByIdController));
-router.post('/', checkRoles(ROLES.TEACHER), validateBody(contactSchema), ctrlWrapper(createContactController));
-router.patch('/:contactId', checkRoles(ROLES.TEACHER, ROLES.PARENT), isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
-router.delete('/:contactId', checkRoles(ROLES.TEACHER), isValidId, ctrlWrapper(deleteContactController));
+router.get('/', ctrlWrapper(getAllContactsController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+router.post('/', validateBody(contactSchema), ctrlWrapper(createContactController));
+router.patch('/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
