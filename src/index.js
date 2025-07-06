946c4a824e257a11dc7aa0605b6d3d4040767ab8
halina_bulochnikova <yatsynkagalina@gmail.com>
2025-07-06 22:15:25 +0200

    add editions files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import dotenv from 'dotenv';
 dotenv.config();
+
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,13 +1,12 @@
 // src/utils/sendMail.js
 
 import nodemailer from 'nodemailer';
-
 import { SMTP } from '../constants/index.js';
-import { getEnvVar } from './getEnvVar.js';
+import { getEnvVar} from './getEnvVar.js';
 
 const transporter = nodemailer.createTransport({
   host: getEnvVar(SMTP.SMTP_HOST),
-  port: Number(getEnvVar(SMTP.SMTP_PORT)),
+  port: 587,
   secure: false,
   auth: {
     user: getEnvVar(SMTP.SMTP_USER),
